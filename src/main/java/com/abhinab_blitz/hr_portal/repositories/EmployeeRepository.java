c0f832c2586bf4d99c2c89297ed4ebbd8da5cab9
dev-blitz <abhinab.duttagupta@gmail.com>
2026-08-21 02:51:34 +0530

    corrected the primary key error changed the model Primary Key datatype from Integer to UUID

--- src/main/java/com/abhinab_blitz/hr_portal/repositories/EmployeeRepository.java
+++ src/main/java/com/abhinab_blitz/hr_portal/repositories/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.abhinab_blitz.hr_portal.repositories;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.abhinab_blitz.hr_portal.models.Employee;
 
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+public interface EmployeeRepository extends JpaRepository<Employee, UUID> {
     Employee findByCompanyEmployeeId(String companyEmployeeId);
     Employee findByCompanyEmail(String companyEmail);
     Employee findByPhoneNumber(String phoneNumber);
